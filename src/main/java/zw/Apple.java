ba533baef06e93ff83740becad5c4b0c6f851960
伊尔叁似舞 <5730006+three_like_dancing@user.noreply.gitee.com>
2020-02-27 18:24:59 +0800

    merge

--- src/main/java/zw/Apple.java
+++ src/main/java/zw/Apple.java
@@ -2,10 +2,10 @@ package zw;
 
 public class Apple {
 	public void sayHello(){
-		System.out.println("hello world");
+		System.out.println("hello world merge");
 	}
 	public void testUpdate(){
-		System.out.println("this is test update");
+		System.out.println("this is merge test update");
 	}
 	public void myBranch(){
 		System.out.println("this is test create merge branch");
